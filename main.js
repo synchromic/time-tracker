288d0df8fdaf381ea2255884391a47f0ae181d44
synchromic <realvince7778@gmail.com>
2025-11-05 10:44:58 -0800

    update port

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.static("static"));
 
 app.get("/", (req, res) => {
   res.sendFile("static/index.html");
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
